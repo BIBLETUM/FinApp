70ae9a7228315dd9217b9e2934eefc39fd5383da
BIBLETUM <pasha.remizov@mail.ru>
2025-06-14 16:25:50 +0300

    fixed FAB on android 16

--- app/src/main/java/com/yanschool/finapp/MainActivity.kt
+++ app/src/main/java/com/yanschool/finapp/MainActivity.kt
@@ -3,6 +3,7 @@ package com.yanschool.finapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.material3.MaterialTheme
 import androidx.core.view.WindowCompat
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
@@ -10,6 +11,7 @@ import com.yanschool.ui.theme.FinAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         WindowCompat.setDecorFitsSystemWindows(window, false)
         setContent {
--- ui/src/main/java/com/yanschool/components/core/FloatingActionButtonPlus.kt
+++ ui/src/main/java/com/yanschool/components/core/FloatingActionButtonPlus.kt
@@ -3,6 +3,7 @@ package com.yanschool.components.core
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Icon
@@ -22,6 +23,7 @@ fun FloatingActionButtonPlus(
 ) {
     Box(
         modifier = modifier
+            .offset(y = 24.dp)
             .size(56.dp)
             .clip(CircleShape)
             .background(
