72c2b6f8081eece6925f88c133ba80c1bd1fcc51
BIBLETUM <pasha.remizov@mail.ru>
2025-06-16 18:42:41 +0300

    added observation of network status

--- app/src/main/java/com/yanschool/finapp/presentation/MainActivity.kt
+++ app/src/main/java/com/yanschool/finapp/presentation/MainActivity.kt
@@ -1,19 +1,39 @@
 package com.yanschool.finapp.presentation
 
+import android.net.ConnectivityManager
+import android.net.Network
+import android.net.NetworkCapabilities
+import android.net.NetworkRequest
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.material3.MaterialTheme
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
+import com.yanschool.finapp.R
 import com.yanschool.ui.theme.FinAppTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    private lateinit var connectivityManager: ConnectivityManager
+
+    private val networkCallback = object : ConnectivityManager.NetworkCallback() {
+        override fun onLost(network: Network) {
+            super.onLost(network)
+            Toast.makeText(
+                this@MainActivity,
+                getString(R.string.no_internet_connection), Toast.LENGTH_LONG
+            ).show()
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
+        setUpConnectivityManager()
         setContent {
             val systemUiController = rememberSystemUiController()
             FinAppTheme {
@@ -22,5 +42,20 @@ class MainActivity : ComponentActivity() {
                 MainScreen()
             }
         }
+    }
+
+    private fun setUpConnectivityManager() {
+        connectivityManager = getSystemService(CONNECTIVITY_SERVICE) as ConnectivityManager
+
+        val networkRequest = NetworkRequest.Builder()
+            .addCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+            .build()
+
+        connectivityManager.registerNetworkCallback(networkRequest, networkCallback)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        connectivityManager.unregisterNetworkCallback(networkCallback)
     }
 }
